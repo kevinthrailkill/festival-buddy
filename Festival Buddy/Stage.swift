e074eb32922f52b3094263bfdc80bcc31c42545b
Kevin Thrailkill <kevinjt92@gmail.com>
2017-03-22 10:10:37 -0700

    Started mock times

--- Festival Buddy/Stage.swift
+++ Festival Buddy/Stage.swift
@@ -31,33 +31,60 @@ func generateQueensDomain() -> Stage {
     fridayDateComponent.year = 2017
     fridayDateComponent.month = 4
     fridayDateComponent.day = 24
-    fridayDateComponent.hour = 7
     fridayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let friday = userCalendar.date(from: fridayDateComponent)
     
+    
+    fridayDateComponent.hour = 8
+    fridayDateComponent.minute = 30
+    var friday = userCalendar.date(from: fridayDateComponent)
     var artist: Artist = Artist(artistName: "Audien", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
     artists.append(artist)
     
+    fridayDateComponent.hour = 5
+    friday = userCalendar.date(from: fridayDateComponent)
+    artist = Artist(artistName: "No Requests", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
+    
+    artists.append(artist)
+    
+    
+    fridayDateComponent.hour = 5
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Cheat Codes", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    
+    fridayDateComponent.hour = 11
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "GTA", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    fridayDateComponent.hour = 9
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Hardwell", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    fridayDateComponent.hour = 7
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Martin Solveig", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    fridayDateComponent.hour = 12
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Sander Van Doorn", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    fridayDateComponent.hour = 6
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Wiwek", sTime: friday!, eTime: friday!, stage: "Queen's Domain")
 
     artists.append(artist)
@@ -68,38 +95,61 @@ func generateQueensDomain() -> Stage {
     saturdayDateComponent.year = 2017
     saturdayDateComponent.month = 4
     saturdayDateComponent.day = 25
-    saturdayDateComponent.hour = 8
     saturdayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let saturday = userCalendar.date(from: saturdayDateComponent)
     
+    
+    saturdayDateComponent.hour = 8
+    saturdayDateComponent.minute = 45
+    var saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Borgeous", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 12
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Diplo", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 10
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Nghtmre", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 7
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Ookay", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 6
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Party Favor", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 5
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "SikDope", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 9
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Tommy Trash", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
     
+    saturdayDateComponent.hour = 11
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Yellow Claw", sTime: saturday!, eTime: saturday!, stage: "Queen's Domain")
 
     artists.append(artist)
@@ -119,24 +169,40 @@ func generateUpsideDownHouse() -> Stage {
     fridayDateComponent.month = 4
     fridayDateComponent.day = 24
     fridayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let friday = userCalendar.date(from: fridayDateComponent)
     
+    
+    fridayDateComponent.hour = 10
+    fridayDateComponent.minute = 30
+    var friday = userCalendar.date(from: fridayDateComponent)
     var artist: Artist = Artist(artistName: "Aazar", sTime: friday!, eTime: friday!, stage: "Upside Down House")
 
     artists.append(artist)
     
+    fridayDateComponent.hour = 5
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Des McMahon", sTime: friday!, eTime: friday!, stage: "Upside Down House")
     artists.append(artist)
     
+    fridayDateComponent.hour = 11
+    fridayDateComponent.minute = 45
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Gladiator", sTime: friday!, eTime: friday!, stage: "Upside Down House")
     artists.append(artist)
     
+    fridayDateComponent.hour = 9
+    fridayDateComponent.minute = 15
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Jstjr", sTime: friday!, eTime: friday!, stage: "Upside Down House")
     artists.append(artist)
     
+    fridayDateComponent.hour = 6
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Rell The Soundbender", sTime: friday!, eTime: friday!, stage: "Upside Down House")
     artists.append(artist)
     
+    fridayDateComponent.hour = 8
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Two Fresh", sTime: friday!, eTime: friday!, stage: "Upside Down House")
     artists.append(artist)
     
@@ -146,20 +212,37 @@ func generateUpsideDownHouse() -> Stage {
     saturdayDateComponent.month = 4
     saturdayDateComponent.day = 25
     saturdayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let saturday = userCalendar.date(from: saturdayDateComponent)
     
+    
+    saturdayDateComponent.hour = 8
+    var saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Bot", sTime: saturday!, eTime: saturday!, stage: "Upside Down House")
+    
     artists.append(artist)
     
+    
+    
+    saturdayDateComponent.hour = 6
+    saturdayDateComponent.minute = 30
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Craig Williams", sTime: saturday!, eTime: saturday!, stage: "Upside Down House")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 7
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Dombresky", sTime: saturday!, eTime: saturday!, stage: "Upside Down House")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 11
+    saturdayDateComponent.minute = 30
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Redlight", sTime: saturday!, eTime: saturday!, stage: "Upside Down House")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 10
+    saturdayDateComponent.minute = 15
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Sinden", sTime: saturday!, eTime: saturday!, stage: "Upside Down House")
     artists.append(artist)
     
@@ -178,28 +261,43 @@ func generateAquarium() -> Stage {
     fridayDateComponent.month = 4
     fridayDateComponent.day = 24
     fridayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let friday = userCalendar.date(from: fridayDateComponent)
     
+    
+    fridayDateComponent.hour = 7
+    fridayDateComponent.minute = 45
+    var friday = userCalendar.date(from: fridayDateComponent)
     var artist: Artist = Artist(artistName: "Angelz", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
+    fridayDateComponent.hour = 10
+    fridayDateComponent.minute = 15
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Cut Snake", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
+    fridayDateComponent.hour = 9
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Jax Jones", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
+    fridayDateComponent.hour = 11
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Rezz", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
+    fridayDateComponent.hour = 6
+    fridayDateComponent.minute = 30
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Sage Armstrong", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
+    fridayDateComponent.hour = 12
+    fridayDateComponent.minute = 45
+    friday = userCalendar.date(from: fridayDateComponent)
     artist = Artist(artistName: "Shiba San", sTime: friday!, eTime: friday!, stage: "The Aquarium")
     artists.append(artist)
     
-    artist = Artist(artistName: "Wongo", sTime: friday!, eTime: friday!, stage: "The Aquarium")
-    artists.append(artist)
     
     //saturday
     var saturdayDateComponent = DateComponents()
@@ -207,26 +305,34 @@ func generateAquarium() -> Stage {
     saturdayDateComponent.month = 4
     saturdayDateComponent.day = 25
     saturdayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let saturday = userCalendar.date(from: saturdayDateComponent)
     
-    artist = Artist(artistName: "Audien", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
-    artists.append(artist)
-    
+    saturdayDateComponent.hour = 10
+    saturdayDateComponent.minute = 15
+    var saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "AC Slater", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 11
+    saturdayDateComponent.minute = 30
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Chris Lake", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
     artists.append(artist)
     
-    artist = Artist(artistName: "Go Freek", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
-    artists.append(artist)
     
+    saturdayDateComponent.hour = 6
+    saturdayDateComponent.minute = 30
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Kry Wolf", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 12
+    saturdayDateComponent.minute = 45
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "MK", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 9
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Will Clarke", sTime: saturday!, eTime: saturday!, stage: "The Aquarium")
     artists.append(artist)
     
@@ -246,8 +352,11 @@ func generateCheshireCove() -> Stage {
     fridayDateComponent.month = 4
     fridayDateComponent.day = 24
     fridayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let friday = userCalendar.date(from: fridayDateComponent)
     
+    
+    fridayDateComponent.hour = 1
+    fridayDateComponent.minute = 45
+    var friday = userCalendar.date(from: fridayDateComponent)
     var artist: Artist = Artist(artistName: "Astrix", sTime: friday!, eTime: friday!, stage: "Cheshire Cove")
     artists.append(artist)
     
@@ -279,26 +388,40 @@ func generateCheshireCove() -> Stage {
     saturdayDateComponent.month = 4
     saturdayDateComponent.day = 25
     saturdayDateComponent.timeZone = TimeZone(abbreviation: "PST")
-    let saturday = userCalendar.date(from: saturdayDateComponent)
     
+    saturdayDateComponent.hour = 10
+    var saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Aly & Fila", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 8
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Darren Porter", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 5
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Daun Giventi", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 11
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Markus Schulz", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 12
+    saturdayDateComponent.minute = 30
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Paul Van Dyk", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 9
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Sean Tyas", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
     
+    saturdayDateComponent.hour = 7
+    saturday = userCalendar.date(from: saturdayDateComponent)
     artist = Artist(artistName: "Waio", sTime: saturday!, eTime: saturday!, stage: "Cheshire Cove")
     artists.append(artist)
 
